d4ad396ea8bf0971e9c61552a7adedae4c7540f4
swss <zaqplm7913@naver.com>
2023-03-11 07:48:33 +0900

    23.03.11

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+
+
+
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 5,
+        spaceBetween: 15,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
+      });
+      
+
+
+
+
+
+
+})
